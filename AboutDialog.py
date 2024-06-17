d5574d35ab0ee88cb4bfabd47e687f749ddc684e
hungmvu44 <90425806+hungmvu44@users.noreply.github.com>
2024-06-17 12:39:29 +1000

    Add files via upload

---
+++ AboutDialog.py
@@ -0,0 +1,34 @@
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+
+
+class AboutDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setFixedWidth(500)
+        self.setFixedHeight(250)
+        self.setWindowTitle("About")
+        QBtn = QDialogButtonBox.StandardButton.Ok
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+        font = QFont("Roboto", 15)
+        font.setBold(True)
+        copyright_label = QLabel("Copyright© by Andy VU 2024")
+        copyright_label.setFont(font)
+        layout = QVBoxLayout()
+        labelpic = QLabel()
+        pixmap = QPixmap('logo.png')
+        pixmap = pixmap.scaledToHeight(120)
+        labelpic.setPixmap(pixmap)
+        labelpic.setFixedWidth(120)
+
+        layout.addWidget(copyright_label)
+        layout.addWidget(labelpic)
+
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
--- templateexam.py
+++ templateexam.py
@@ -1,4 +1,3 @@
-
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
@@ -8,8 +7,10 @@ from os import path
 from PyQt6.uic import loadUiType
 import sqlite3
 from datetime import timedelta
-
+from AboutDialog import AboutDialog
 FORM_CLASS,_=loadUiType(path.join(path.dirname('__file__'),"exameditor.ui"))
+
+
 
 class Main(QMainWindow, FORM_CLASS):
     def __init__(self):
@@ -18,7 +19,10 @@ class Main(QMainWindow, FORM_CLASS):
         self.setupUi(self)
         self.setWindowIcon(QIcon('logo.png'))
         
-        
+        about_menu = self.menuBar().addMenu("&About")
+        about_action = QAction("Developer", self)
+        about_action.triggered.connect(self.show_about)
+        about_menu.addAction(about_action)
         ####### Setup exam room table #########
         examroom = self.findChild(QTableWidget,"examroom_table")
         examroom.setStyleSheet('background-color:#ECE75F')
@@ -40,34 +44,37 @@ class Main(QMainWindow, FORM_CLASS):
         
         self.session_menu = self.findChild(QComboBox, "session_menu")
         self.schedule_table = self.findChild(QTableWidget, "schedule_table")
+        self.schedule_table.setStyleSheet('background-color: #CCEBCB')
         self.schedule_table.setFont(font)
         self.schedule_table.setColumnWidth(0,80)
         self.save_btn = self.findChild(QPushButton, "btn_save_schedule")
-        # self.save_btn.clicked.connect()
-        self.time_id_lineEdit = self.findChild(QLineEdit, "time_id")
         ##set up minutes from 0 to 59
-        mins = []
-        for min in range(0,60):
-            mins.append(min)
-            
-        self.setup_min.addItems([str(i) for i in mins])
-        self.read_min.addItems([str(i) for i in mins])
-        self.write_min.addItems([str(i) for i in mins])
-        self.stop_min.addItems([str(i) for i in mins])
+        mins = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+
+        self.setup_min.addItems([i for i in mins])
+        self.read_min.addItems([i for i in mins])
+        self.write_min.addItems([i for i in mins])
+        self.stop_min.addItems([i for i in mins])
         self.connect_database()
         ###Initialise time
         self.load_examsession()
         self.save_btn.clicked.connect(self.insert_examsession)
+        self.ChangeSession.clicked.connect(self.load_examsession)
         
         ####### Time for set up ,read write stop #########
         
         # ####### END Time for set up ,read write stop #########
-   
+    
+    
+    def show_about(self):
+        dlg = AboutDialog()
+        dlg.exec()
+        
     def load_examsession(self):
         try: 
-            db = sqlite3.connect("mhsexam.db", timeout=1)
+            session = self.session_menu.currentText()
             cursor1 = db.cursor()
-            query1 = ''' Select * from Exam_Session '''
+            query1 = f''' Select * from Exam_Session WHERE session= {session} '''
             result = cursor1.execute(query1)
             self.schedule_table.setRowCount(0)
             for row_number, row_data in enumerate(result):
@@ -76,14 +83,15 @@ class Main(QMainWindow, FORM_CLASS):
                     self.schedule_table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
         except sqlite3.Error as e:
             print("Error occurred", e)
-        finally:
-            cursor1.close()
+        
             
 
     def insert_examsession(self):
-        db = sqlite3.connect("mhsexam.db")
-        cursor = db.cursor()
         session = self.session_menu.currentText()
+        cursor = db.cursor()
+        
+        ##### Calculate the duration of the exam
+        
         setup_time = timedelta(hours=float(self.setup_hr.currentText()), minutes=float(self.setup_min.currentText()))
         read_time = timedelta(hours=float(self.read_hr.currentText()), minutes=float(self.read_min.currentText()))
         written_time = timedelta(hours=float(self.write_hr.currentText()), minutes=float(self.write_min.currentText()))
@@ -100,18 +108,20 @@ class Main(QMainWindow, FORM_CLASS):
         stop_hr = self.stop_hr.currentText()
         stop_min = self.stop_min.currentText()
 
-        
         setup = f"{set_up_hr}:{set_up_min}"
         read = f"{read_hr}:{read_min}"
         write = f"{written_hr}:{written_min}"
         stop = f"{stop_hr}:{stop_min}"
         
         row = (session, setup, read, write, stop, duration)
+        
         query = """ INSERT INTO Exam_Session (session, setup_time, read_time, write_time, stop_time, duration)
                     VALUES (?,?,?,?,?,?)"""
         
         cursor.execute(query,row)
         db.commit()
+       
+        
         self.setup_hr.setCurrentIndex(0)
         self.setup_min.setCurrentIndex(0)
         self.read_hr.setCurrentIndex(0)
@@ -125,9 +135,8 @@ class Main(QMainWindow, FORM_CLASS):
        
                     
     def connect_database(self):
-            db = sqlite3.connect("mhsexam.db")
+           
             cursor = db.cursor()
-            
             query = ''' Select * from classroom '''
             result = cursor.execute(query)
             self.examroom_table.setRowCount(0)
@@ -136,9 +145,8 @@ class Main(QMainWindow, FORM_CLASS):
                 for column_number, data in enumerate(row_data):
                     self.examroom_table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
 
-
-
-
+## Connect to database
+db = sqlite3.connect("mhsexam.db", timeout=1)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Main()
